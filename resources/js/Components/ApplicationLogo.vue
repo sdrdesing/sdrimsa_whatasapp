3471b4612765cb7b1ab46cf8bef71f515285c76f
jhuaman818 <jhuaman818@gmail.com>
2026-01-03 10:50:08 -0500

    correcion del logo

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,7 +1,7 @@
 
 <template>
     <img
-        src="/logos/sdrimsac.png"
+        :src="logoSrc"
         :alt="alt"
         :style="{ width: computedSize, height: computedSize }"
         class="object-contain"
@@ -9,7 +9,14 @@
 </template>
 
 <script>
+import logoSrc from '../../../../../../public/logos/sdrimsac.png';
+
 export default {
+    data() {
+        return {
+            logoSrc,
+        };
+    },
     props: {
         // size can be a number (pixels) or any valid CSS size string (e.g. '4rem', '100px')
         size: {
