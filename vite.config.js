c4fc43ff93fdecd5d0c7d999fc08b2bf349fe47c
warriorcmd <pezo.exe@gmail.com>
2026-02-17 10:59:41 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,66 +2,33 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-/* export default defineConfig(({ mode }) => ({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: false, // sin HMR en producción
-        }),
-        vue(),
-    ],
-    build: {
-        outDir: 'public/build',
-        emptyOutDir: true,
-        manifest: 'manifest.json',
-        rollupOptions: {
-            input: 'resources/js/app.js',
-            output: {
-                dir: 'public/build',
-            },
-        },
-        // Configuración para assets estáticos
-        assetsDir: 'assets',
-        assetsInlineLimit: 0,
-    },
-    base: '/build/',
-    server: process.env.NODE_ENV === 'development' ? {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-        hmr: false,
-        watch: { usePolling: true, interval: 100 },
-    } : undefined,
-})); */
-
-export default defineConfig(({ mode }) => {
-  const isProd = mode === 'production'
-
-  return {
-    plugins: [
-      laravel({
-        input: ['resources/js/app.js'],
-        refresh: true, // en dev sí refresca
-      }),
-      vue(),
-    ],
-
-    // IMPORTANTE: base solo en producción
-    base: isProd ? '/build/' : '/',
-
-    server: {
-      host: '0.0.0.0',     // para que escuche dentro del container
-      port: 5173,
-      strictPort: true,
-
-      // CLAVE: esto es lo que verá el navegador (no 0.0.0.0)
-      hmr: {
-        host: 'sdrimsacbot.test',  // o 'localhost' si entras por localhost
-        port: 5173,
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    laravel({
+      input: 'resources/js/app.js',
+      refresh: false,
+    }),
+    vue(),
+  ],
+  build: {
+    outDir: 'public/build',
+    emptyOutDir: true,
+    manifest: 'manifest.json',
+    rollupOptions: {
+      input: 'resources/js/app.js',
+      output: {
+        dir: 'public/build',
       },
-
-      // opcional si usas polling en Windows/WSL/docker
-      watch: { usePolling: true, interval: 100 },
     },
-  }
-});
+    assetsDir: 'assets',
+    assetsInlineLimit: 0,
+  },
+  base: '/build/',
+  server: process.env.NODE_ENV === 'development' ? {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: false,
+    watch: { usePolling: true, interval: 100 },
+  } : undefined,
+}));
