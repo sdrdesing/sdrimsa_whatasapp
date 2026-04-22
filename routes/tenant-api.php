d7e134ffde2d5c51f9c12b0a37cdcc9a043b48ed
warriorcmd <pezo.exe@gmail.com>
2026-04-22 16:39:48 -0500

    rutas nuevas

--- routes/tenant-api.php
+++ routes/tenant-api.php
@@ -8,4 +8,6 @@ Route::prefix('whatsapp')->group(function () {
     Route::get('/qr', [WhatsappController::class, 'qr']);
     Route::post('/send', [WhatsappController::class, 'send']);
     Route::post('/send_media', [WhatsappController::class, 'send_media']);
+    Route::post('/send-group', [WhatsappController::class, 'send_to_group']);
+    Route::post('/send-group-media', [WhatsappController::class, 'send_media_to_group']);
 });
